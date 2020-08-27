caeda51289c740699b10edc7fda346918defde33
Ziyang Li <liby99@seas.upenn.edu>
2020-08-27 02:45:57 -0400

    Fixed reference

--- src/timeout.hpp
+++ src/timeout.hpp
@@ -22,7 +22,7 @@ namespace to {
     std::mutex m;
     std::condition_variable cv;
     ResultType result;
-    std::thread t([&cv, &f, &result, args...]() {
+    std::thread t([&cv, &f, &result, &args...]() {
       result = f(args...);
       cv.notify_one();
     });
@@ -51,7 +51,7 @@ namespace to {
   ) {
     std::mutex m;
     std::condition_variable cv;
-    std::thread t([&cv, &f, args...]() {
+    std::thread t([&cv, &f, &args...]() {
       f(args...);
       cv.notify_one();
     });
